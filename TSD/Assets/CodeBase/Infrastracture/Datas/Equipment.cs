500f5bdbd5900a9aec3f31e402ba8d5c2c2d2aa7
Dron26 <91642819+Dron26@users.noreply.github.com>
2023-12-25 16:57:48 +0300

    CorrectScripts

--- TSD/Assets/CodeBase/Infrastracture/Datas/Equipment.cs
+++ TSD/Assets/CodeBase/Infrastracture/Datas/Equipment.cs
@@ -1,21 +1,3 @@
-using System;
-using Unity.VisualScripting;
-
-namespace CodeBase.Infrastracture.Datas
-{
-    [Serializable]
-    public class Equipment
-    {
-        [Serialize] public string ShortSerial => _shortSerial;
-        [Serialize] public string SerialNumber => _serialNumber;
-
-        private string _shortSerial;
-        private string _serialNumber;
-
-        public Equipment(string serialNumber)
-        {
-            _serialNumber = serialNumber;
-            _shortSerial = _serialNumber[^4..];
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d8a9cf8f24e58812216d248cf9ade4b35cf7b7936c0dcf6899b4d000d8b2e94
+size 595
